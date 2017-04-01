e93a8515a2b8009f6f379ac467a65c1e52ed04c4
Lumiral <Airakunnn@gmail.com>
2017-04-01 13:59:15 -0700

    Beta Code

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+
+
+namespace Djeeta_Bot
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Djeeta granblue = new Djeeta();
+        }
+    }
+}
